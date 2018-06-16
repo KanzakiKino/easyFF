e852681421b9ae63828199d3d3cc24f521e7d8f3
KanzakiKino <kino@knzk.work>
2018-06-16 21:14:22 +0900

    [add] Added FFRational.

---
+++ include/fftypes.h
@@ -0,0 +1,14 @@
+// Written under LGPL-3.0 in the C language.
+// Copyright 2018 KanzakiKino
+
+#ifndef EASYFF_FFTYPES_H
+#define EASYFF_FFTYPES_H
+
+#include "fferror.h"
+
+typedef struct
+{
+    int num, den;
+} FFRational;
+
+#endif
